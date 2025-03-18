81b0061c7bb4f74a63bbc93002b9c97c0296159e
omar nazih <omarnazihcs@gmail.com>
2025-03-18 02:14:07 +0200

    removed unused imports and formatted imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, Route, RouterProvider, createRoutesFromElements, BrowserRouter as Router, Routes, } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 
 import App from './App';
 import Login from './pages/Login';
